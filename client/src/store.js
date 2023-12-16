2e04b3834ed42b5ee9873f2c1f2e00e3fb2ea80b
Caden Midkiff <cadenmidkiff32@gmail.com>
2023-12-16 16:42:28 -0700

    Improve mobile view and icons

--- client/src/store.js
+++ client/src/store.js
@@ -10,7 +10,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 
 export default store;
