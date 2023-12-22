811a232171e092ded1f4db5409bb0ef9459ecb56
Caden Midkiff <cadenmidkiff32@gmail.com>
2023-12-22 00:56:00 -0700

    prod server fixes

--- prod-server.js
+++ prod-server.js
@@ -24,7 +24,7 @@ app.use("/api/leaderboard", require("./routes/api/leaderboard"));
 app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "public"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
